3af4e28f296bb9c203965da0c957fdf31257c7a0
RobinTheissen <robin.theissen00@gmail.com>
2023-11-16 20:57:07 +0100

    part booleans beendet

---
+++ booleans-tasks.js
@@ -0,0 +1,13 @@
+const hour = 23
+
+const name = 'Robin'
+
+if (hour >=6 && hour <13) {
+    console.log(`Good morning ${name}`);
+}
+else if (hour >=13 && hour <18) {
+    console.log(`Good afternoon ${name}`)
+}
+else if (true) {
+    console.log(`Good night ${name}`)
+} 
