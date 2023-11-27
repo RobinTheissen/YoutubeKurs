c43a14f44df83f1e85a66e8bc3a1c015f418b158
RobinTheissen <robin.theissen00@gmail.com>
2023-11-27 22:35:48 +0100

    Schere Stein Papier spiel nun mit functions

--- schere-stein-papier.js
+++ schere-stein-papier.js
@@ -1,40 +1,59 @@
-function schereSteinPapier() {
-
-    const randomNr = Math.random();
-
-    let computerMove = '';
-
-    if (randomNr >= 0 && randomNr < 1/3){
-        let computerMove = 'Schere';
-        console.log(computerMove);
-    }
-
-    else if (randomNr >= 1/3 && randomNr < 2/3){
-        let computerMove = 'Stein';
-        console.log(computerMove);
-    }
-
-    else if (randomNr >= 2/3 && randomNr < 1){
-        let computerMove = 'Papier';
-        console.log(computerMove);
-    }
-
-    
-
+function playGame(playerMove) {
+    const computerMove = pickComputerMove();
 
     let result = '';
 
-    if (computerMove === 'Schere'){
-        result = 'Tie.'
+    if (playerMove === 'Schere') {
+        if (computerMove === 'Schere'){
+            result = 'Unentschieden';
+        }
+        else if(computerMove === 'Stein'){
+            result = 'Verloren';
+        }
+        else if(computerMove === 'Papier'){
+            result = 'Gewonnen';
+        }
+    }
+    else if (playerMove === 'Stein') {
+        if (computerMove === 'Stein'){
+            result = 'Unentschieden';
+        }
+        else if(computerMove === 'Paper'){
+            result = 'Verloren';
+        }
+        else if(computerMove === 'Schere'){
+            result = 'Gewonnen';
+        }
+    }
+    else if (playerMove === 'Papier') {
+        if (computerMove === 'Stein'){
+            result = 'Gewonnen';
+        }
+        else if(computerMove === 'Papier'){
+            result = 'Unentschieden';
+        }
+        else if(computerMove === 'Schere'){
+            result = 'Verloren';
+        }
+    }
+    alert(`Du hast ${playerMove} gewählt. Gegner hat ${computerMove} gewählt. ${result}.`)
+}
+
+
+function pickComputerMove() {
+
+    const randomNr = Math.random();
+    
+    if (randomNr >= 0 && randomNr < 1/3){
+        computerMove = 'Schere';
     }
 
-    else if(computerMove === 'Stein'){
-        result = 'you loose'
+    else if (randomNr >= 1/3 && randomNr < 2/3){
+        computerMove = 'Stein';
     }
 
-    else if(computerMove === 'Paper'){
-        result = 'you won'
+    else if (randomNr >= 2/3 && randomNr < 1){
+        computerMove = 'Papier';
     }
-
-    alert(`You picked rock. Computer picked ${computerMove} ${result}`);
+    return computerMove;
 }
