875068d1dac6497ec4ea27540e90346166b287c4
RobinTheissen <robin.theissen00@gmail.com>
2023-12-03 22:44:09 +0100

    aufgaben zu objects bearbeitet

---
+++ tasks/Shop.js
@@ -0,0 +1,55 @@
+let cartQuantity = 0;
+
+function resetCart () {
+    cartQuantity = 0;
+      console.log('Cart was reset.');
+      console.log(`Cart quantity: ${cartQuantity}`);
+}
+// Funktion um den Warenkorb wieder auf 0 zu setzen
+
+
+function updateCartQunatity(input) {
+    if (cartQuantity + input > 10) {
+      alert('The Cart is full');
+      return
+    } 
+    if (cartQuantity + input < 0) {
+      alert('Not enough items in the cart');
+      return
+    }
+    
+    cartQuantity += input;
+    console.log(`Cart quanitity: ${cartQuantity}`)
+    
+  }
+  // Funktion um Produkte den Warenkorb hinzuzufügen und zu entfernen.
+  // Maximal 10, Mindestens 0
+
+
+function showQuantity () {
+    console.log(`Cart quantity: ${cartQuantity}`);
+  }
+  // Gibt die aktuelle Menge der Produkte des Warenkorbs zurück.
+
+
+const product1 = {
+    name: 'Basketball',
+    price: 2095,
+}
+
+const product2 = {
+    name: 'Football',
+    price: 3000
+}
+
+
+function comparePrice (product1, product2) {
+    if (product1.price < product2.price) {
+        return product1
+        
+    }
+    if (product1 > product2) {
+        return product2
+    }
+}
+console.log(comparePrice(product1, product2))
