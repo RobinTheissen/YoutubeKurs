b0a4051d8d1382a5eab9760e8d450ef200b386c8
RobinTheissen <robin.theissen00@gmail.com>
2024-01-22 16:04:34 +0100

    bisschen

--- kursinhalt/09-dom.js
+++ kursinhalt/09-dom.js
@@ -1,32 +1,72 @@
 // buttonFunctionality.js
-function toggleSubscription() {
-    const buttonElement = document.querySelector('.js-subscribe-button');
+function toggle() {
+    const buttonElements = document.querySelector('.js-subscribe-button');
 
-    if (buttonElement.innerHTML === 'Subscribe') {
-        buttonElement.innerHTML = 'Subscribed';
+    if (buttonElements.innerHTML === 'Subscribe') {
+        buttonElements.innerHTML = 'Subscribed';
     } else {
-        buttonElement.innerHTML = 'Subscribe';
+        buttonElements.innerHTML = 'Subscribe';
     }
 }
 
-// Event-Listener hinzufügen, der auf das Laden des DOMs wartet
-document.addEventListener('DOMContentLoaded', function() {
-    // Das Element mit der Klasse '.js-subscribe-button' auswählen
-    const subscribeButton = document.querySelector('.js-subscribe-button');
 
-    // Event-Listener für das Klick-Ereignis des ausgewählten Elements hinzufügen
-    subscribeButton.addEventListener('click', toggleSubscription);
+document.addEventListener('DOMContentLoaded', function() {
+    const subscribeButton = document.querySelector('.js-subscribe-button');
+    subscribeButton.addEventListener('click', toggle);
 });
+
+
 
 
 
 function calculateFees() {
     const inputElement = document.querySelector('.js-input');
-    let cost = inputElement.value;
+    let cost = parseFloat(inputElement.value);
+    
     
     if (cost < 40) {
         cost = cost + 10
     }
 
     document.querySelector('.js-fees').innerHTML = `${cost}€`
+}
+
+
+//9b
+function changeButton() {
+    const buttonElements = document.querySelector('.button-9a');
+
+    if (buttonElements.innerHTML === '9a') {
+        buttonElements.innerHTML = 'done';
+    } else {
+        buttonElements.innerHTML = '9a';
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const subscribeButton = document.querySelector('.button-9a');
+    subscribeButton.addEventListener('click', changeButton);
+});
+
+
+function changeButtons() {
+    const buttonElements = document.querySelector('.button-9b');
+
+    if (buttonElements.innerHTML === '9b') {
+        buttonElements.innerHTML = 'fertig';
+    } else {
+        buttonElements.innerHTML = '9b';
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const subscribeButton = document.querySelector('.button-9b');
+    subscribeButton.addEventListener('click', changeButtons);
+});
+
+
+function choose(option) {
+    document.getElementsByClassName('fill').innerHTML = 'You chose: ' + option
 }
