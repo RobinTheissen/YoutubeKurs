726ca45ba3e77ff654d86519599499c889f233d6
RobinTheissen <robin.theissen00@gmail.com>
2024-01-30 22:20:48 +0100

    bisschen

---
+++ kursinhalt/11-arrays-loops.js
@@ -0,0 +1,18 @@
+const nums = [1, 1, 3,]
+let total = 0
+
+for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    total += num;
+}
+
+console.log(total)
+
+const numsDoubled = [];
+
+for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    numsDoubled.push(num * 2);
+}
+
+console.log(numsDoubled)
--- todo-list/todo.js
+++ todo-list/todo.js
@@ -1,4 +1,23 @@
-const todoList = []
+const todoList = ['make dinner', 'wäsche', 1]
+
+renderTodoList()
+
+function renderTodoList() {
+    let todoListHTML = ""
+
+    for (let i = 0; i < todoList.length; i++) {
+        const todo = todoList[i]
+        const html = `<p>${todo}</p>`
+        todoListHTML += html
+    }
+    console.log(todoListHTML)
+    
+    
+    document.querySelector('.js-msg').innerHTML = todoListHTML
+}
+
+
+
 
 function addTodo() {
     const inputElement = document.querySelector('.input')
@@ -9,4 +28,5 @@ function addTodo() {
 
     inputElement.value = ""
 
+    renderTodoList();
 }
